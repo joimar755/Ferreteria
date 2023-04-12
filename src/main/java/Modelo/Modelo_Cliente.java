ed472ad1e619870fa1fa9e131db55d6443385ef5
joimar755 <joimarjose19@gmail.com>
2023-04-12 01:50:22 -0500

    MMMMM1

--- src/main/java/Modelo/Modelo_Cliente.java
+++ src/main/java/Modelo/Modelo_Cliente.java
@@ -10,22 +10,26 @@ import java.util.Date;
  *
  * @author joimar
  */
-public class Modelo_Cliente extends Modelo_Persona{
-    private String Telefono;
-    private String Correo; 
-    private String Direccion;
+public class Modelo_Cliente extends Modelo_Persona {
 
-    public Modelo_Cliente(String Telefono, String Correo, String Direccion, int Id, String Nombre, String Apellido, int Cedula, Date Fecha_Nac) {
+    private String Telefono;
+    private String Correo;
+    private String Direccion;
+    private int Persona_Id;
+
+    public Modelo_Cliente(String Telefono, String Correo, String Direccion, int Id, String Nombre, String Apellido, int Cedula, Date Fecha_Nac, int Persona_Id) {
         super(Id, Nombre, Apellido, Cedula, Fecha_Nac);
         this.Telefono = Telefono;
         this.Correo = Correo;
         this.Direccion = Direccion;
+        this.Persona_Id = Persona_Id;
     }
 
-    public Modelo_Cliente(String Telefono, String Correo, String Direccion) {
+    public Modelo_Cliente(String Telefono, String Correo, String Direccion, int Persona_Id) {
         this.Telefono = Telefono;
         this.Correo = Correo;
         this.Direccion = Direccion;
+        this.Persona_Id = Persona_Id;
     }
 
     public String getTelefono() {
@@ -51,6 +55,13 @@ public class Modelo_Cliente extends Modelo_Persona{
     public void setDireccion(String Direccion) {
         this.Direccion = Direccion;
     }
-    
-    
+
+    public int getPersona_Id() {
+        return Persona_Id;
+    }
+
+    public void setPersona_Id(int Persona_Id) {
+        this.Persona_Id = Persona_Id;
+    }
+
 }
--- src/main/java/Modelo/Modelo_Productos.java
+++ src/main/java/Modelo/Modelo_Productos.java
@@ -8,6 +8,72 @@ package Modelo;
  *
  * @author joimar
  */
-public class Modelo_Productos {
+public class Modelo_Productos{
+    private int  Productos_id;
+    private String Nombre;
+    private int Precio_Unit;
+    private int stock;
+    private int Providers_id;
+    private int Cliente_id;
+
+    public Modelo_Productos(int Productos_id, String Nombre, int Precio_Unit, int stock, int Providers_id, int Cliente_id) {
+        this.Productos_id = Productos_id;
+        this.Nombre = Nombre;
+        this.Precio_Unit = Precio_Unit;
+        this.stock = stock;
+        this.Providers_id = Providers_id;
+        this.Cliente_id = Cliente_id;
+    }
+
+    public Modelo_Productos() {
+    }
+
+    public int getProductos_id() {
+        return Productos_id;
+    }
+
+    public void setProductos_id(int Productos_id) {
+        this.Productos_id = Productos_id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getPrecio_Unit() {
+        return Precio_Unit;
+    }
+
+    public void setPrecio_Unit(int Precio_Unit) {
+        this.Precio_Unit = Precio_Unit;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getProviders_id() {
+        return Providers_id;
+    }
+
+    public void setProviders_id(int Providers_id) {
+        this.Providers_id = Providers_id;
+    }
+
+    public int getCliente_id() {
+        return Cliente_id;
+    }
+
+    public void setCliente_id(int Cliente_id) {
+        this.Cliente_id = Cliente_id;
+    }
     
 }
